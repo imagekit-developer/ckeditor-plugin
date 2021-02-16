9bc08738a1203b4e761981883daa9ad7ccb40c99
nikniv <niveditn@gmail.com>
2021-02-16 12:22:59 +0530

    include core plugin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ButtonView from '@ckeditor/ckeditor5-ui/src/button/buttonview';
 // This SVG file import will be handled by webpack's raw-text loader.
 // This means that imagekitLogo will hold the source SVG.
 import imagekitLogo from './../theme/icons/imagekit-logo.svg';
+import IKMediaLibraryWidgetCore from 'imagekit-media-library-widget';
 
 export class ImagekitMediaLibraryWidget extends Plugin {
     init() {
@@ -10,6 +11,37 @@ export class ImagekitMediaLibraryWidget extends Plugin {
 
         editor.ui.componentFactory.add('imagekitMediaLibraryWidget', locale => {
             const view = new ButtonView(locale);
+
+            const pluginOptions = editor.config.get('imagekitMediaLibraryWidget.config');
+
+            // attach to editor context and initialize core widget plugin 
+            IKMediaLibraryWidgetCore.call(this);
+
+            // media-library-widget plugin
+            const config = {
+                name: pluginOptions.name,
+                container: pluginOptions.container,
+                className: pluginOptions.className,
+                dimensions: {
+                    height: pluginOptions.dimensions.height,
+                    width: pluginOptions.dimensions.width,
+                },
+                view: 'modal',
+                showOpenButton: false,
+            };
+
+            function callback(payload) {
+                // consume json payload
+                payload.data.map(({ name, url }) => {
+                    // insert images at cursor position
+                    editor.model.change(writer => {
+                        const insertPosition = editor.model.document.selection.getFirstPosition();
+                        writer.insertElement('image', { src: url, alt: name }, insertPosition);
+                    });
+                });
+            }
+
+            new IKMediaLibraryWidget(config, callback);
 
             view.set({
                 label: 'ImageKit Media Library',
